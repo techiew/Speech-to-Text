849fabdd86a2e8a8c51b565bad22057427cf1a7c
Unknown <Stormstrike87@gmail.com>
2019-02-13 11:13:01 +0100

    Nye klasser. Nye navn.
    Programmet tar nå inn en fil (må mekkes fra GUI eller kommandolinje, er statisk nå), laster den opp og skriver ut en transcription i ett kjør.

--- src/main.java
+++ src/main.java
@@ -2,7 +2,6 @@
 public class main {
 
 	public static void main(String[] args) throws Exception {
-		String gcsUri = "gs://goodest_team_lydfiler/LangInspillWAV.wav";
-		new ITX(gcsUri);
+		new GetFile();
 	}
 }
